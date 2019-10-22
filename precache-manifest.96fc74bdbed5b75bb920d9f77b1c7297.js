5c7ed1a88bf87818dba32ec8b99809428e7e6002
Christian <trnchristian94@gmail.com>
2019-10-22 13:46:27 +0200

    Updates

---
+++ precache-manifest.96fc74bdbed5b75bb920d9f77b1c7297.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b850c80fc333f158511f70c01531bb72",
+    "url": "/chriscvreact/index.html"
+  },
+  {
+    "revision": "ceaef074071614b7b7cc",
+    "url": "/chriscvreact/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "68d3bf1293cb30c01a5c",
+    "url": "/chriscvreact/static/css/main.19f7b497.chunk.css"
+  },
+  {
+    "revision": "ceaef074071614b7b7cc",
+    "url": "/chriscvreact/static/js/2.7affc447.chunk.js"
+  },
+  {
+    "revision": "68d3bf1293cb30c01a5c",
+    "url": "/chriscvreact/static/js/main.8ce7882d.chunk.js"
+  },
+  {
+    "revision": "300db862ca997ce6e8a0",
+    "url": "/chriscvreact/static/js/runtime-main.3d9e5a5b.js"
+  }
+]);
