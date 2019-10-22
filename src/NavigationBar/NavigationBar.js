46803a4d4f60f114e0526d7d7ff33aec3bf10318
Christian <trnchristian94@gmail.com>
2019-10-22 12:16:06 +0200

    Initial commit

---
+++ src/NavigationBar/NavigationBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
+import { NavLink, withRouter } from "react-router-dom";
+
+export default class NavigationBar extends React.Component {
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Christian T.R.</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Information</Nav.Link>
+          <Nav.Link href="#pricing">Comments</Nav.Link>
+        </Nav>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-info">Search</Button>
+        </Form>
+      </Navbar>
+    );
+  }
+}
